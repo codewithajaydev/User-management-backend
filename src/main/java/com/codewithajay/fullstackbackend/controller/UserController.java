ee8a87db9635cd3574b20b13ad12bc63e1dac663
codewithajaydev <ajaybecse03@gmail.com>
2025-08-17 18:59:31 +0530

    Update UserController.java

--- src/main/java/com/codewithajay/fullstackbackend/controller/UserController.java
+++ src/main/java/com/codewithajay/fullstackbackend/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
